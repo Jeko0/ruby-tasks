667cbe97d99fa6680c8eee76388e94647ecebea1
exit <exit>
2022-03-28 12:40:09 -0700

    magic numbers to constans and gave a proper name for a new object

--- area/task2.rb
+++ area/task2.rb
@@ -1,16 +1,20 @@
 # frozen_string_literal: true
-
+# solution for triangle area
 class Triangle
+  FORMULA_1 = 1
+  FORMULA_2 = 2
   def initialize(base, height)
-    @a = base
-    @h = height
+    @a = base.to_f
+    @h = height.to_f
   end
 
   def area_of_triangle
-    (@a * @h) / 2
+    @a * @h * FORMULA_1 / FORMULA_2
   end
 end
-abc = Triangle.new(, )
-#choose the examples you like
 
-puts abc.area_of_triangle 
+puts 'Enter Triangles base and height'
+base = gets.chomp
+height = gets.chomp
+triangle1 = Triangle.new(base, height)
+puts triangle_1.area_of_triangle
