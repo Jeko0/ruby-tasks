8ec76e19240cd3539c43a4b1049ecb1e173da29b
Jeko0 <japulava@unisens.ge>
2022-03-28 12:03:20 +0400

    solution for second task

---
+++ area/task2.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Triangle
+  def initialize(base, height)
+    @a = base
+    @h = height
+  end
+
+  def area_of_triangle
+    (@a * @h) / 2
+  end
+end
+abc = Triangle.new(, )
+#choose the examples you like
+
+puts abc.area_of_triangle 
