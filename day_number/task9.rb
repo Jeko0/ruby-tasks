56164ce332c2663378f46ee58758cfec0eb2a7b6
exit <exit>
2022-03-31 12:56:48 -0700

    modified code with strftime method

--- day_number/task9.rb
+++ day_number/task9.rb
@@ -9,10 +9,10 @@ class RegularDay
     @day = day
   end
 
-  def date_to_number
-    "Day #{Date.new(@year, @month, @day).yday} of the year"
+  def what_day_of_the_year_is_it?
+    "Day #{Date.new(@year, @month, @day).strftime("%j")} of the year"
   end
 end
 
 # put any input you like
-puts RegularDay.new(2022, 3, 12).date_to_number
+puts RegularDay.new(2022, 4, 22).what_day_of_the_year_is_it?
