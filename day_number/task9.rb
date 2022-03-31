150cbf308c36aa19e7810cf7ff9fc68e34fed5e5
exit <exit>
2022-03-31 05:54:05 -0700

    task8 convert day to number

---
+++ day_number/task9.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "date"
+
+class RegularDay
+  def initialize(year, month, day)
+    @year = year
+    @month = month
+    @day = day
+  end
+
+  def date_to_number
+    "Day #{Date.new(@year, @month, @day).yday} of the year"
+  end
+end
+
+# put any input you like
+puts RegularDay.new(2022, 3, 12).date_to_number
