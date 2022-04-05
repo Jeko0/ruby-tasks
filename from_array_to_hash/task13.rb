d40dc09b6d57a5583340b6f532ecf169ee428394
Jeko0 <japulava@unisens.ge>
2022-04-05 15:17:20 +0400

    task13 transfrom array to hash

---
+++ from_array_to_hash/task13.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+COMPUTER_LANGUAGES = %w[Ruby Python Java .NET HTML JavaScript].freeze
+
+class ArrayhashValue
+  def call
+    tranform_array_to_hash
+  end
+
+  private
+
+  def tranform_array_to_hash
+    COMPUTER_LANGUAGES.each_with_object({}) do |lang, hash|
+      hash[lang] = lang.length
+    end
+  end
+end
+
+puts ArrayhashValue.new.call
