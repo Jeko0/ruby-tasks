cff3c694a9786eed5792934a04ae9ac2eb316410
Jeko0 <japulava@unisens.ge>
2022-04-05 14:36:06 +0400

    task12 find the longest word in array

--- week4_longest_word/task12.rb
+++ week4_longest_word/task12.rb
@@ -3,29 +3,12 @@
 COMPUTER_LANGUAGES = %w[Ruby Python Java .NET HTML Javascript].freeze
 
 class LongestWordFinder
-  class EmptyarrayError < StandardError; end
-
-  def find_longest_word_in_array
+  def find_longest_word_in_arr
     COMPUTER_LANGUAGES.max_by(&:length)
   end
 
   def call
-    something_wrong?
-    display_longest_word
-  rescue EmptyarrayError => e
-    warn e
-  end
-
-  private
-
-  def something_wrong?
-    return unless COMPUTER_LANGUAGES.empty?
-
-    raise EmptyarrayError, 'Looks like an array is empty'
-  end
-
-  def display_longest_word
-    find_longest_word_in_array
+    find_longest_word_in_arr
   end
 end
 
