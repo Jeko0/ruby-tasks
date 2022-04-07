0319ba0a74519cb9f95d23d91ce7e2080ca29470
Jeko0 <japulava@unisens.ge>
2022-04-07 15:10:12 +0400

    find factorial of number

---
+++ factorial/factorial_finder.rb
@@ -0,0 +1,66 @@
+frozen_string_literal: true
+
+# frozen_string_literal: true
+
+FACTORIAL_START = 1
+
+class FactorialFinder
+  def initialize(num)
+    @num = num
+  end
+
+  def call
+    input_validation
+    find_factorial_of_number
+  rescue ArgumentError => e
+    warn e
+  end
+
+  private
+
+  def find_factorial_of_number
+    if @num <= FACTORIAL_START
+      FACTORIAL_START
+    else
+      (FACTORIAL_START..@num).inject(:*)
+    end
+  end
+
+  def input_validation
+    raise ArgumentError, "invalid input" unless @num.is_a?(Integer) && (@num.positive? || @num.zero?)
+  end
+end
+
+puts FactorialFinder.new(10).call
+
+
+# with recursion
+
+PREVIOUS_NUM = 1
+LIMIT = 0
+
+class Factorial
+  def initialize(input)
+    @input = input
+  end
+
+  def call
+    input_validation
+    factorial_with_recursion(@input)
+  rescue InputOutputError => e
+    warn e
+  end
+
+  private
+
+  def factorial_with_recursion(num)
+    PREVIOUS_NUM if num == LIMIT
+    num * factorial_with_recursion(num - PREVIOUS_NUM)
+  end
+
+  def input_validation
+    raise ArgumentError, "Invalid Input" unless @input.is_a?(Integer) && (@input.positive? || @input.zero?)
+  end
+end
+
+puts Factorial.new(10).call
