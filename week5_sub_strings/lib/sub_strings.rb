addb190027f93d7be924bdb0064146842c9a6f13
Jeko0 <japulava@unisens.ge>
2022-04-20 12:05:00 +0400

    removed long line and double nesting

--- week5_sub_strings/lib/sub_strings.rb
+++ week5_sub_strings/lib/sub_strings.rb
@@ -6,6 +6,7 @@ class SubstringsFinder
   def initialize(sentence, dictionary)
     @sentence = sentence
     @dictionary = dictionary
+    @substrings = Hash.new(0)
   end
 
   def call
@@ -17,23 +18,28 @@ class SubstringsFinder
 
   private
 
-  def calculate_sub_strings
-    substrings = Hash.new(0)
-    user_input = @sentence.split(" ")
-    user_input.each do |userword|
-      @dictionary.each do |word|
-        substrings[word] += 1 if userword.downcase.include?(word)
-      end
+  def loop(userword)
+    @dictionary.each do |word|
+      @substrings[word] += 1 if userword.downcase.include?(word)
     end
-    substrings
+  end
+
+  def calculate_sub_strings
+    user_input = @sentence.split(' ')
+    user_input.each { |userword| loop(userword) }
+    @substrings
+  end
+
+  def is_valid?
+    @sentence.is_a?(String) && @dictionary.all? { |elemet| elemet.is_a?(String) }
   end
 
   def check_input
-    raise InvalidinputError, "invalid input" unless @sentence.is_a?(String) && @dictionary.all?{|elemet| elemet.is_a?(String)}
+    raise InvalidinputError, 'invalid input' unless is_valid?
   end
 end
 
 sentence = "Howdy partner, sit down, how's it going?"
 dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
-  puts SubstringsFinder.new(sentence, dictionary).call
+puts SubstringsFinder.new(sentence, dictionary).call
