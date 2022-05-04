0cfddd323775ccdd1a6ec48804cafcc5d99dd5c7
Jeko0 <japulava@unisens.ge>
2022-05-04 12:16:49 +0400

    implemet trie with add, search and list

--- trie/lib/trie.rb
+++ trie/lib/trie.rb
@@ -1,70 +1,63 @@
-# frozen_string_literal: true
+class TrieNode
+  attr_accessor :prefix_count, :is_end, :children
 
-class Trie
-  def initialize(end_node = false)
-    @root = {}
-    @end_node = end_node
-    @arr = []
-  end
-
-  def add(word)
-    node = @root
-
-    0.upto(word.length - 1) do |i|
-      char = word[i]
-      node[char] = {} unless node[char]
-      node = node[char]
-    end
-    node[@end_node] = true
-    @arr << word
-  end
-
-  def find(word)
-    node = @root
-
-    0.upto(word.length - 1) do |i|
-      char = word[i]
-      return false unless node[char]
-
-      node = node[char]
-    end
-    node[@end_node] == true
-  end
-
-  def include?(prefix)
-    node = @root
-    prefix.each_char do |letter|
-      # tried to remove double nesting but couldn't :)
-      if node.include?(letter)
-        node = node[letter]
-      else
-        return false
-      end
-    end
-    true
-  end
-
-  def list(prefix)
-    container = []
-    @arr.each do |word|
-      container << word if word.start_with?(prefix)
-    end
-    container.empty? ? false : container
-  end
-
-  def delete(word)
-    @arr.delete(word) if find(word)
-    puts "#{word} has been deleted"
+  def initialize(prefix_count = 0, is_end = false)
+    @prefix_count = prefix_count
+    @is_end = is_end
+    @children = Array.new(26)
   end
 end
 
-object = Trie.new
-object.add('cat')
-object.add('cape')
-object.add('cow')
-object.add('captain')
-object.add('bear')
-object.add('bunny')
-object.add('jeko')
+class Trie
+  attr_accessor :head
 
-# puts object.list("c")
+  def initialize
+    @head = TrieNode.new
+  end
+
+  def add(word)
+    word_chars = word.chars
+    current = @head
+    current.prefix_count += 1
+    (0..word.length - 1).each do |i|
+      letter = word_chars[i].ord - 'a'.ord
+      current.children[letter] = TrieNode.new if current.children[letter].nil?
+      current.children[letter].prefix_count += 1
+      current = current.children[letter]
+    end
+    current.is_end = true
+  end
+
+  def find(word)
+    word_chars = word.chars
+    current = @head
+    (0..word.length - 1).each do |i|
+      letter = word_chars[i].ord - 'a'.ord
+      return false if current.children[letter].nil?
+
+      current = current.children[letter]
+    end
+    current.is_end
+  end
+
+  def list(prefix)
+    prefix_chars = prefix.chars
+    current = @head
+    (0..prefix.length - 1).each do |i|
+      letter = prefix_chars[i].ord - 'a'.ord
+      return 0 if current.children[letter].nil?
+
+      current = current.children[letter]
+    end
+    current.prefix_count
+  end
+end
+
+trie = Trie.new
+trie.add('cat')
+trie.add('cape')
+trie.add('cow')
+trie.add('camp')
+trie.add('cave')
+
+p trie.list('cat')
